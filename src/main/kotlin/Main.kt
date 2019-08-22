e3243bce5538ae7a6493c2455624a4f6d0d2fae3
iseki <admin@iseki.space>
2019-08-22 23:16:02 +0800

    goodbye

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,58 +1,96 @@
-import api.func.restrictChatMember
-import api.func.sendMessage
+import api.func.*
 import api.type.ChatPermissions
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.vertx.core.Future
 import io.vertx.core.Vertx
 import io.vertx.core.http.HttpServerOptions
-import io.vertx.core.impl.ConcurrentHashSet
 import io.vertx.core.json.Json
+import io.vertx.core.json.JsonObject
 import io.vertx.core.net.ProxyOptions
-import io.vertx.core.net.ProxyType
+import io.vertx.ext.web.client.WebClient
 import io.vertx.ext.web.client.WebClientOptions
+import io.vertx.ext.web.multipart.MultipartForm
+import java.io.File
+import java.io.FileReader
+import java.io.FileWriter
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.collections.HashMap
+import kotlin.math.max
 
-val userSet = ConcurrentHashSet<User>()
-val keyMap = ConcurrentHashMap<String, User>()
+val uuidMap = ConcurrentHashMap<String, User>()
+val limitMap = ConcurrentHashMap<Long, Int>()
 
 var botServer: BotServer? = null
+var webClient: WebClient? = null
+val storageFile = System.getenv("STORAGE_FILE") ?: throw Exception("STORAGE_FILE is not set.")
+val botKey = System.getenv("RECAPTCHA_BOT_API_KEY") ?: throw RuntimeException("RECAPTCHA_BOT_API_KEY is not set.")
+
+
 fun main() {
-    val botKey = System.getenv("RECAPTCHA_BOT_API_KEY") ?: throw RuntimeException("RECAPTCHA_BOT_API_KEY is not set.")
     Json.mapper.registerKotlinModule().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     val vertx = Vertx.vertx()
+
+    if (File(storageFile).exists()) {
+        load()
+    }
+
+    // create webClient
+    webClient = WebClient.create(
+        vertx,
+        WebClientOptions().setProxyOptions(ProxyOptions(JsonObject(System.getenv("PROXY_SETTING"))))
+    )
 
     // create http server for recaptcha verify request.
     val server = vertx.createHttpServer(HttpServerOptions().setPort(8000))
     server.requestHandler {
         val token = it.getParam("token")
-        val key = it.getParam("key")
-        val user = keyMap[key]
+        val uuid = it.getParam("uuid")
+        val user = uuidMap[uuid]
         if (user != null) {
-            if (verifyToken(token)) {
-                unlockUser(user)
+            verifyToken(token).setHandler {
+                if (it.result() == true) {
+                    unlockUser(user)
+                }
             }
         }
+        it.response().setStatusCode(302).setStatusMessage("Temporary Redirect")
+            .putHeader("Location", "https://recaptcha.utau.name/done.html")
+            .end()
+        save()
     }
 
     // bot service.
     botServer = BotServerImpl(
         vertx,
         botKey,
-        WebClientOptions().setProxyOptions(ProxyOptions().setType(ProxyType.SOCKS5).setHost("127.0.0.1").setPort(17654))
+        WebClientOptions().setProxyOptions(ProxyOptions(JsonObject(System.getenv("PROXY_SETTING"))))
     )
     botServer!!.registerNewMessageHandler { msg ->
         //println(msg)
+
         msg.newChatMembers?.forEach {
             if (it.isBot) {
                 // TODO -> kick
             } else {
                 lockUser(it, msg.chat.id.toString())
             }
+            botServer!!.getApiContext().deleteMessage(msg.chat.id.toString(), msg.messageId)
+            save()
         }
+
         true
     }
+    uuidMap.forEach { (uuid, user) ->
+        val kd = user.kickDate
+        println("""kick user: ${user.firstName}   ${max(kd.time - Date().time, 1)}""")
+        vertx.setTimer(max(kd.time - Date().time, 1)) {
+            kickUser(uuid, user.chatId)
+        }
+    }
+
     MsgUpdaterImpl(botServer!!).start()
 }
 
@@ -62,14 +100,13 @@ fun lockUser(tgUser: api.type.User, chatId: String) {
     val user = User(
         uid = tgUser.id,
         status = State.LOCKED,
-        key = uuid,
+        uuid = uuid,
         chatId = chatId,
         firstName = tgUser.firstName,
-        lastName = tgUser.lastName ?: ""
+        lastName = tgUser.lastName ?: "",
+        kickDate = Calendar.getInstance().apply { add(Calendar.MINUTE, limitMap[chatId.toLong()] ?: 10) }.time
     )
-    userSet.add(user)
-    keyMap[uuid] = user
-    //println("kkkk$chatId")
+    putUser(uuid, user)
     botServer!!.getApiContext().restrictChatMember(
         chatId = chatId,
         permissions = ChatPermissions(
@@ -78,31 +115,32 @@ fun lockUser(tgUser: api.type.User, chatId: String) {
             canSendOtherMessages = false
         ),
         userId = tgUser.id
-    ) {
-        if (it.succeeded()) {
-            botServer!!.getApiContext().sendMessage(
-                chatId = chatId,
-                text = """
+    ).compose {
+        botServer!!.vertx.setTimer((limitMap[chatId.toLong()] ?: 10) * 60 * 1000L) {
+            kickUser(uuid, chatId)
+        }
+        botServer!!.getApiContext().sendMessage(
+            chatId = chatId,
+            text = """
                     *>>>注意<<<*
                     User: [${tgUser.firstName} ${tgUser.lastName}](tg://user?id=${tgUser.id})
                     User id: `${tgUser.id}`
                     Username: `${tgUser.username}`
-                    阁下已被自动禁言，请在*300*秒内通过[此链接](https://recaptcha.utau.name/groupJavaer.html?uuid=$uuid)的验证码，否则你将被*请离*！
-                    You're restricted, please check the [RECAPTCHA code](https://recaptcha.utau.name/groupJavaer.html?uuid=$uuid) in *300* seconds. Or you will be kicked.
+                    阁下已被自动禁言，请在*${limitMap[chatId.toLong()]
+                ?: 10}*分钟内通过[此链接](https://recaptcha.utau.name/?uuid=$uuid)的验证码，否则你将被*请离*！
+                    You're restricted, please check the [RECAPTCHA code](https://recaptcha.utau.name/?uuid=$uuid) in *300* seconds. Or you will be kicked.
                     #新人锁定
                 """.trimIndent(),
-                parseMode = "Markdown"
-            ) {
-                if (it.failed()) {
-                    println("bbb")
-                    botError(chatId, it.cause(), tgUser.id.toString())
-                    println(it.cause())
-                }
+            parseMode = "Markdown"
+        ).apply {
+            botServer!!.vertx.setTimer((limitMap[chatId.toLong()] ?: 10) * 1000L) {
+                kickUser(uuid, chatId)
+                botServer!!.getApiContext().deleteMessage(chatId, this@apply.result()?.result?.messageId ?: 0)
             }
-        } else {
-            println("aaa")
-            botError(chatId, it.cause(), tgUser.id.toString())
-            println(it.cause())
+        }
+    }.setHandler {
+        if (it.failed()) {
+            botError(chatId, it.cause(), user.uid.toString())
         }
     }
 }
@@ -121,20 +159,16 @@ fun unlockUser(user: User) {
             canPinMessages = true,
             canSendPolls = true
         )
-    ) {
-        if (it.failed()) {
-            botError(user.chatId, it.cause(), user.uid.toString())
-            println(it.cause())
-        }
-    }
-    botServer!!.getApiContext().sendMessage(
-        chatId = user.chatId,
-        parseMode = "Markdown",
-        text = """
+    ).compose {
+        botServer!!.getApiContext().sendMessage(
+            chatId = user.chatId,
+            parseMode = "Markdown",
+            text = """
             *欢迎* [${user.firstName} ${user.lastName}](tg://user?id=${user.uid}) 加入本群组，请阅读群介绍和置顶消息：）
             #通过验证
         """.trimIndent()
-    ) {
+        )
+    }.setHandler {
         if (it.failed()) {
             botError(user.chatId, it.cause(), user.uid.toString())
             println(it.cause())
@@ -142,18 +176,62 @@ fun unlockUser(user: User) {
     }
 }
 
-fun verifyToken(token: String): Boolean {
+fun verifyToken(token: String): Future<Boolean> = Future.future { promise ->
+    val form = MultipartForm.create()
+    form.attribute("secret", System.getenv("RECAPTCHA_KEY") ?: throw RuntimeException("RECAPTCHA_KEY is not set."))
+    form.attribute("response", token)
+    webClient!!.postAbs("https://www.google.com/recaptcha/api/siteverify")
+        .sendMultipartForm(form) {
+            if (it.succeeded()) {
+                promise.tryComplete(it.result().bodyAsJsonObject().getBoolean("success"))
+            } else {
+                promise.tryFail(it.cause())
+                println(it.cause())
+            }
+        }
+}
 
-    TODO()
+fun kickUser(uuid: String, chatId: String) {
+    val u = uuidMap[uuid] ?: return
+    delUser(uuid)
+    botServer!!.getApiContext().sendMessage(
+        chatId = chatId,
+        parseMode = "Markdown",
+        text = """
+            *Kick User:* [${u.firstName} ${u.lastName}](tg://user?id=${u.uid})
+            #kick
+        """.trimIndent()
+    ).compose { result ->
+        save()
+        botServer!!.vertx.setTimer(20000) {
+            if (result?.result?.messageId != null) {
+                botServer!!.getApiContext().deleteMessage(
+                    chatId = chatId,
+                    messageId = result.result.messageId
+                )
+            }
+        }
+        botServer!!.getApiContext().kickChatMember(
+            chatId = chatId,
+            userId = u.uid
+        )
+    }.compose {
+        botServer!!.getApiContext().unbanChatMember(chatId, u.uid)
+    }.setHandler {
+        if (it.failed()) {
+            botError(chatId, it.cause(), u.uid.toString())
+        }
+    }
 }
 
 data class User @JsonCreator constructor(
     val uid: Int,
     val status: State,
-    val key: String,
+    val uuid: String,
     val chatId: String,
     val firstName: String,
-    val lastName: String
+    val lastName: String,
+    val kickDate: Date
 )
 
 enum class State {
@@ -168,7 +246,48 @@ fun botError(chatId: String, exception: Throwable, uid: String) {
                     Bot异常： ${exception.message}
                     涉及uid: [$uid](tg://user?id=$uid)
                     #bot异常
-                """.trimIndent(),
-        disableNotification = true
+                """.trimIndent().replace(botKey, "******"),
+        disableNotification = true,
+        parseMode = "Markdown"
     )
 }
+
+data class Storage(val groups: List<Group>)
+
+data class Group(val chatId: Long, val users: List<User>, val timeLimit: Int)
+
+fun save() {
+    val map = HashMap<Long, LinkedList<User>>()
+    uuidMap.forEach { (_, user) ->
+        val chatId = user.chatId
+        map.getOrPut(chatId.toLong(), { LinkedList() }).add(user)
+    }
+    val list = map.map { (chatId, users) ->
+        Group(chatId = chatId, timeLimit = limitMap[chatId] ?: 10, users = users)
+    }.toList()
+    val a = JsonObject.mapFrom(Storage(list))
+    FileWriter("$storageFile.new").apply {
+        write(a.encode())
+        close()
+    }
+    File(storageFile).delete()
+    File("$storageFile.new").renameTo(File(storageFile))
+    println("File saved success.")
+}
+
+fun load() {
+    val file = FileReader(storageFile)
+    val a = JsonObject(file.readText()).mapTo(Storage::class.java)
+    file.close()
+    a.groups.forEach {
+        limitMap[it.chatId] = it.timeLimit
+        it.users.forEach {
+            uuidMap[it.uuid] = it
+        }
+    }
+}
+
+fun putUser(uuid: String, user: User) = uuidMap.put(uuid, user)
+
+fun delUser(uuid: String) =
+    uuidMap.remove(uuid)
